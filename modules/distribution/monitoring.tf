245f88730f0ad5d421bfc85b3e9449aee16ff2a4
Byungjin Park (Claud) <posquit0.bj@gmail.com>
2022-08-20 17:00:36 +0900

    Fix monitoring subscription issue (#2)

--- modules/distribution/monitoring.tf
+++ modules/distribution/monitoring.tf
@@ -3,11 +3,13 @@
 ###################################################
 
 resource "aws_cloudfront_monitoring_subscription" "this" {
+  count = var.monitoring_realtime_metrics_enabled ? 1 : 0
+
   distribution_id = aws_cloudfront_distribution.this.id
 
   monitoring_subscription {
     realtime_metrics_subscription_config {
-      realtime_metrics_subscription_status = var.monitoring_realtime_metrics_enabled ? "Enabled" : "Disabled"
+      realtime_metrics_subscription_status = "Enabled"
     }
   }
 }
